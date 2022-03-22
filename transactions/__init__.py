ed994b65468e8ea30f91fcc20540a13a7bd2ee46
Samarth <samarthr1@outlook.com>
2022-03-22 14:47:34 +0530

    Added timestamp to transactions

--- transactions/__init__.py
+++ transactions/__init__.py
@@ -1,3 +1,8 @@
+import datetime
+import sqlite3
+
+from fastapi import HTTPException
+
 from database import get_db
 from models import TransactionData, TransactionResponse
 
@@ -6,16 +11,24 @@ def create_transaction(transdata: TransactionData) -> TransactionResponse:
     try:
         conn = get_db()
         cur = conn.cursor()
-        cur.execute("INSERT INTO Transactions (Amount, Remarks, A1, A2) VALUES (? ,? ,?, ?) RETURNING Id",
-                    (transdata.Amount, transdata.Remarks, transdata.A1, transdata.A2))
+        current_time = datetime.datetime.utcnow().timestamp()
+        cur.execute(
+            "INSERT INTO Transactions (Amount, Remarks, A1, A2, TransactionTime) VALUES (? ,? ,?, ?, ?) RETURNING Id",
+            (transdata.Amount, transdata.Remarks, transdata.A1, transdata.A2, current_time))
         row_id = cur.fetchone()[0]
         cur.executemany("UPDATE Account SET Amount=Amount+? WHERE Id=?",
                         [(-transdata.Amount, transdata.A1), (transdata.Amount, transdata.A2)])
-        # cur.executemany("SELECT Amount FROM Account WHERE Id = ?", [(transdata.A1,), (transdata.A2,)])
-        cur.execute("SELECT Id, Amount FROM Account WHERE Id = ? OR Id = ?", (transdata.A1, transdata.A2))
+        cur.execute("SELECT Id, Amount FROM Account WHERE Id = ? OR Id = ?", (transdata.A1, transdata.A2,))
         data = cur.fetchall()
-        amounts = {data[0]["Id"]: data[0]["Amount"], data[1]["Id"]: data[1]["Amount"]}
+        if len(data) != 2:
+            raise HTTPException(status_code=400, detail="NO_ACCOUNT")
+        else:
+            amounts = {data[0]["Id"]: data[0]["Amount"], data[1]["Id"]: data[1]["Amount"]}
         conn.commit()
         return TransactionResponse(Id=row_id, Bal1=amounts[transdata.A1], Bal2=amounts[transdata.A2])
+    except sqlite3.Error as e:
+        print(e)
+        raise HTTPException(status_code=500, detail="DB_ERROR")
     except Exception as e:
         raise e
+
