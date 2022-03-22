965d60316157c3ca49073f5c8989276a91276218
Samarth <samarthr1@outlook.com>
2022-03-22 12:25:09 +0530

    fixed create_transaction

--- transactions/__init__.py
+++ transactions/__init__.py
@@ -9,9 +9,13 @@ def create_transaction(transdata: TransactionData) -> TransactionResponse:
         cur.execute("INSERT INTO Transactions (Amount, Remarks, A1, A2) VALUES (? ,? ,?, ?) RETURNING Id",
                     (transdata.Amount, transdata.Remarks, transdata.A1, transdata.A2))
         row_id = cur.fetchone()[0]
-        cur.executemany("SELECT Amount FROM Account WHERE Id = ?", [(transdata.A1,), (transdata.A2,)])
+        cur.executemany("UPDATE Account SET Amount=Amount+? WHERE Id=?",
+                        [(-transdata.Amount, transdata.A1), (transdata.Amount, transdata.A2)])
+        # cur.executemany("SELECT Amount FROM Account WHERE Id = ?", [(transdata.A1,), (transdata.A2,)])
+        cur.execute("SELECT Id, Amount FROM Account WHERE Id = ? OR Id = ?", (transdata.A1, transdata.A2))
         data = cur.fetchall()
+        amounts = {data[0]["Id"]: data[0]["Amount"], data[1]["Id"]: data[1]["Amount"]}
         conn.commit()
-        return TransactionResponse(Id=row_id, Bal1=data[0][0], Bal2=data[1][0])
+        return TransactionResponse(Id=row_id, Bal1=amounts[transdata.A1], Bal2=amounts[transdata.A2])
     except Exception as e:
         raise e
